ca2e825eacd0e6543b8b6f76eece5ea16589b564
DLindquist <dannyl2131@gmail.com>
2023-07-24 19:50:05 -0600

    finished resume section and added links

--- portfolio2.0/src/components/Footer.js
+++ portfolio2.0/src/components/Footer.js
@@ -6,9 +6,11 @@ export default function Footer(){
   <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
     <div className="col-md-4 d-flex align-items-center">
       <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-        Sample
+        Danny Lindquist
       </a>
-      <span className="mb-3 mb-md-0 text-muted">&copy; 2022 Company, Inc</span>
+      <span className="mb-3 ml-3 mb-md-0 text-muted"><a href='https://github.com/dannyl2131'>Github</a>  <a href='www.linkedin.com/in/daniel-lindquist0a921ab'>LinkedIn</a> <a href='/'>Dice</a></span>
+      <span className="mb-3 ml-3 mb-md-0 text-muted">  </span>
+      <span className="mb-3 ml-3 mb-md-0 text-muted"></span>
     </div>
 
     <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
--- portfolio2.0/src/components/Resume.js
+++ portfolio2.0/src/components/Resume.js
@@ -1,5 +1,27 @@
+import resume from '../images/Daniel_Lindquist_resume.docx'
+
 export default function Resume() {
     return (
-    <h1>THIS IS THE RESUME PAGE</h1>
+    <div className='container'>
+    <h3>Download Resume <a href={resume}>Here</a></h3>
+    <h2>Front end proficiencies</h2>
+    <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+        <li>React</li>
+        <li>Jquery</li>
+        <li>Bootstrap</li>
+        <li>Responsive design</li>
+    </ul>
+    <h2>Back end proficiencies</h2>
+    <ul>
+        <li>Express</li>
+        <li>Node.js</li>
+        <li>SQL/Sequelize</li>
+        <li>MongoDB/Mongoose</li>
+        <li>GraphQL</li>
+    </ul>
+    </div>
     )
 }
